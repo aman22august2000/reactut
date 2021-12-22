bf7cb02adc67ad224d680279be2bc6df87f30228
Aman Kumar Gupta <54740374+aman22august2000@users.noreply.github.com>
2021-12-22 23:05:48 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <h2></h2>
+  <h1> COVID 19 Corona Virus Tracker </h1>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
